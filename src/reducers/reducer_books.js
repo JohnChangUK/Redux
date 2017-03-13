df4b4a49a1db9a1d68d13ade5af673d0f2200249
John Chang <johnchang9094@gmail.com>
2017-03-13 23:10:22 +0000

    Defined Reducers in reducer_book file

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -4,5 +4,8 @@ export default function() {
     {title: "Design Patterns"},
     {title: "Cracking the Code Interview"},
     {title: "Eloquent Ruby"}
-  ]
+  ];
 }
+
+// Reducers take in  1. the action (info about what happened)
+// 2. Copy of the current state
