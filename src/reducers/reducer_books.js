146a8428710dd3b560d364dac3fd972b5ffe7871
John Chang <johnchang9094@gmail.com>
2017-03-29 00:08:33 +0100

    Reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: "You don't know JS"},
+    {title: "Design Patterns"},
+    {title: "Cracking the Code Interview"},
+    {title: "Eloquent Ruby"}
+  ]
+}
